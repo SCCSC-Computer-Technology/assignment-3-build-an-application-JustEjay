45b38a4994820b85bf591a5eed77c078fbccb0e0
Ejay <ericjhowardjr@gmail.com>
2024-02-08 20:40:02 -0500

    Add files via upload

---
+++ StateClassLibary/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StateClassLibary
+{
+    public class ErrorMessage
+    {
+       
+        public string inValidData(string dataType)
+        {
+            return $"You have entered invalid data type for {dataType}";
+        }
+        
+        public string noRow()
+        {
+            return $"Row has not been selected";
+        }
+     
+        public string noEntry()
+        {
+            return $"You have not selected a column";
+        }
+       
+    }
+
+    
+}
